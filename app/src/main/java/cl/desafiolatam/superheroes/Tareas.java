d755ed883fdfa5193c926282165af79f1f2f4eba
Elvis <evalderramapoblete@gmail.com>
2022-03-29 15:12:21 -0300

    Proyecto Inicial

---
+++ app/src/main/java/cl/desafiolatam/superheroes/Tareas.java
@@ -0,0 +1,33 @@
+//[] Consumo de API
+//        [] dependencias
+//        [] permiso de internet
+//[ ] POJOs
+//        [] Interfaz de operaciones
+//[] cliente de retrofit
+//[ ] Repositorio
+//[ ] Presenter para listado
+//        [ ] Interfaz para callbacks de la vista
+//        [ ] Interfaz para callbacks del repositorio
+//[ ] Presenter para detalle
+//        [ ] Interfaz para callbacks de la vista
+//        [ ] Interfaz para callbacks del repositorio
+//[ ] Fragmento de listado
+//        [ ] Layout de item list
+//        [ ] Adapter + ViewHolder + RV
+//        [ ] Consumo de imágenes (dependencias)
+//        [ ] Instanciar presentador
+//[ ] Fragmento de detalle
+//        [ ] Instanciar presentador
+//[ ] Pruebas unitarias
+//        [ ] Pruebas unitarias al presentador del listado
+//[ ] Verificación de buenas prácticas
+//[ ] Textos en strings.xml
+//[ ] Dimensiones en dimens.xml
+//[ ] Colores en colors.xml
+//[ ] No código comentado
+
+
+        package cl.desafiolatam.superheroes;
+
+public class Tareas {
+}
